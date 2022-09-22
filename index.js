97590ea4c3d9a7ce641080a30cc911f1655f5afe
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-22 16:19:05 -0300

    start graphql

--- index.js
+++ index.js
@@ -2,16 +2,46 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const routes = require('./routes')
-app.use(bodyParser.json())
+const {ApolloServer, gql} = require('apollo-server-express')
 
-//routes/products
+//RESTAPIs
+app.use(bodyParser.json())
 app.use( routes)
 
 
+//GRAPHQL
+const typeDefs = gql`
+type Query {
+    getAllProducts: String
+}
+`
+const resolvers = {
+    Query: {
+        getAllProducts: () => 'All products'
+    }
+}
+const graphqlServer = new ApolloServer({
+typeDefs,
+resolvers
+})
+
+
+
+
+//link graphqil with APP and start server
+graphqlServer.start().then(res => {
+    graphqlServer.applyMiddleware({app})
+    
 app.listen(3000,(err)=> {
     if(err){
         console.log('not possible listen on port 3000')
     }else{
         console.log('Server runnig...')
     }
+  })
+
 })
+
+
+
+
